e7fa1617518382997ac1c23cf90480aebb97eeb5
Arthur Tavares <arthurabreu00@gmail.com>
2019-06-28 20:48:48 -0300

    Tanto faz

--- Atividade1.sql
+++ Atividade1.sql
@@ -8,6 +8,9 @@ SELECT nome_prop,estado_prop FROM propietario WHERE estado_prop IN ("MG","SP","R
 
 -- 3.	Relação de todos os dados dos veículos cujo valor do IPVA é 800, em ordem da placa.
 
+
+-- COMENTARIO PARA TESTE;
+
 SELECT * FROM veiculo WHERE vlrIpva_vei = 800 ORDER BY placa_vei;
 
 
